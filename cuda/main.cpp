3e93a66d946d2c9dd1831d130f9c84ba4f53e993
brookicv <brookicv@gmail.com>
2021-12-17 18:40:13 +0800

    test fmp

---
+++ cuda/main.cpp
@@ -0,0 +1,9 @@
+/*** 
+ * @Author: Lq
+ * @Date: 2021-12-17 18:39:03
+ * @LastEditTime: 2021-12-17 18:39:03
+ * @LastEditors: Lq
+ * @Description: 
+ */
+
+#include "3rd/fmt-8.0.1/include/fmt/format.h"
